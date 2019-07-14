1cf8629f424473abd76c1754d35a6944f85484f0
Purvi <pandyapurvi134@yahoo.in>
2019-07-15 03:28:17 +1000

    creating server with grapgql

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,12 +1,49 @@
 const graphql = require('graphql');
+const_ = require('lodash');
 
-const(GraphQLObjectType, GraphQLString) = graphql;
+const {GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt} = graphql;
 
 const BookType = new GraphQLObjectType({
   name: 'Book',
   fields: () => ({
-    id: { type:GraphQLString },
+    id: { type:GraphQLID },
     name: {type: GraphQLString},
     genre: {type: GraphQLString}
   })
+});
+
+const AuthorType = new GraphQLObjectType({
+  name: 'Author',
+  fields: () => ({
+    id: { type:GraphQLID },
+    name: {type: GraphQLString},
+    age: {type: GraphQLInt}
+  })
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    book: {
+      type: BookType,
+      args: { id: {type: GraphQLID}},
+      resolve(parent, args){
+
+        //code to get the data from db/other source
+        console.log(typeof(args.id));
+        return _.find(books, {id: args.id});
+      }
+    },
+    author: {
+      type: AuthorType,
+      age: {id:{type:GraphQLID}},
+      resolve(parent, args){
+        return _.find(authors, {id: args.id});
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
 });
